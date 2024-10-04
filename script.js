4ba6e29cbc3ec1b294d9a1a95866d63259cc0454
nadun-sankalpa <nadunsankalpadesilva27@gmail.com>
2024-10-04 14:43:43 +0530

    latest submit

--- script.js
+++ script.js
@@ -85,7 +85,27 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*==================== typed js ====================*/
 
+/*=====================circle skills====================*/
 
+const circles = document.querySelectorAll('.circle');
+circles.forEach(elem=>{
+    var dots = elem.getAttribute('data-dots');
+    var marked = elem.getAttribute('data-percent');
+    var percent = Math.floor((dots * marked) / 100);
+    var points = "";
+    var rotate = 360 / dots;
+
+
+    for (var i = 0; i < dots; i++) {
+        points += `<div class="points" style="--i:${i};--rot:${rotate}deg;"></div>`;
+    }
+    elem.innerHTML = points;
+
+    const pointsMarked = elem.querySelectorAll('.points');
+    for(let i = 0; i < percent; i++){
+        pointsMarked[i].classList.add('marked');
+    }
+})
 
 
 
